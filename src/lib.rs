7da2e1e5dc0d47c5cf28974303f52ce174557d9e
Abhinav Praveen <abhinav@praveen.org.uk>
2023-05-20 22:37:48 +0100

    Add WG interface creation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
-use std::process::Command;
+use std::process::{Command,ExitStatus};
+
+pub fn create_wg(name: &str) -> ExitStatus {
+    Command::new("ip")
+        .args(["link", "add", "dev", name, "type", "wireguard"])
+        .spawn().expect("Failed run ip, do you have it installed?")
+        .wait().expect("Failed to wait on child, something went very wrong.")
+}
+
+pub fn exitmsg(msg : String, s : ExitStatus) {
+    if let Some(n) = s.code() {
+        panic!("{} {})", msg, n);
+    } else {
+        panic!("{} No exit code, terminated by a signal?)", msg)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod lib;
+
+use crate::lib::*;
+
 fn main() {
-    println!("Hello, world!");
+    let wg_name = "test";
+
+    println!("Creating wireguard interface.");
+    let create_wg_status = create_wg(&wg_name);
+    
+    if create_wg_status.success() {
+        println!("Created wireguard interace {}", wg_name);
+    } else {
+        exitmsg(format!("Failed to create wireguard interface {} (ip:", wg_name), create_wg_status)
+    }
 }
