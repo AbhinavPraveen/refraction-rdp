5b763055ecdafecbca8f62572a05027e2a8039e3
Abhinav Praveen <abhinav@praveen.org.uk>
2023-05-21 04:46:16 +0100

    Rustfmt.

--- src/bin/refraction-rdp-priv.rs
+++ src/bin/refraction-rdp-priv.rs
@@ -1,5 +1,8 @@
 use refraction_rdp::*;
-use std::{io::{Write, BufRead}, thread};
+use std::{
+    io::{BufRead, Write},
+    thread,
+};
 
 fn main() {
     let wg_name = "wg-refraction";
@@ -9,7 +12,8 @@ fn main() {
     let mut wg_addr = "";
     let sock_path = "/tmp/refraction-rdp.sock";
 
-    let req_sock = create_sock(sock_path).expect(format!("Failed to create Unix Socket at {}", sock_path).as_str());
+    let req_sock = create_sock(sock_path)
+        .expect(format!("Failed to create Unix Socket at {}", sock_path).as_str());
     println!("Created request socket at {}", sock_path);
 
     loop {
@@ -17,30 +21,35 @@ fn main() {
             //This handling can be changed after rust-lang/rust #42839
             let mut pid = String::new();
 
-            let mut req_stream = req_sock.accept()
+            let mut req_stream = req_sock
+                .accept()
                 .expect(format!("Failed to accept stream at {}", sock_path).as_str())
                 .0;
 
             {
-                let reader = req_stream.try_clone().expect("Could not clone request steam.");
+                let reader = req_stream
+                    .try_clone()
+                    .expect("Could not clone request steam.");
                 let mut reader = std::io::BufReader::new(reader);
-                reader.read_line(&mut pid)
+                reader
+                    .read_line(&mut pid)
                     .expect("Failed to get data from request stream.");
                 pid.pop();
                 let req_type = pid.pop();
                 if req_type == Some('s') {
-                    println("Request type is server ({})", req_type)
+                    println("Request type is server ({})", req_type);
                     wg_addr = wg_serv_addr;
                 } else {
-                    println("Request type is client ({})", req_type)
+                    println("Request type is client ({})", req_type);
                     wg_addr = wg_cli_addr;
                 }
-
             }
 
-                    //.req_stream.read_to_string(&mut pid).expect("Failed to get data from request stream.");
-            println!("Received request to provide a wireguard interface to pid: {}", pid);
-            
+            //.req_stream.read_to_string(&mut pid).expect("Failed to get data from request stream.");
+            println!(
+                "Received request to provide a wireguard interface to pid: {}",
+                pid
+            );
 
             println!("Creating wireguard interface.");
             {
@@ -48,18 +57,29 @@ fn main() {
                 if create_wg_status.success() {
                     println!("Created wireguard interace {}", wg_name);
                 } else {
-                    exitmsg(format!("Failed to create wireguard interface {} (ip:", wg_name), create_wg_status);
+                    exitmsg(
+                        format!("Failed to create wireguard interface {} (ip:", wg_name),
+                        create_wg_status,
+                    );
                 }
-
             }
 
             println!("Configuring wireguard interface with wg.");
             {
                 let wg_conf_wg_status = wg_conf_wg(&wg_name, &wg_conf);
                 if wg_conf_wg_status.success() {
-                    println!("Configured wireguard interface {} with wg and {}.", &wg_name, &wg_conf)
+                    println!(
+                        "Configured wireguard interface {} with wg and {}.",
+                        &wg_name, &wg_conf
+                    )
                 } else {
-                    exitmsg(format!("Failed to configure wireguard interface {} with wg and {} (wg:", &wg_name, &wg_conf), wg_conf_wg_status)
+                    exitmsg(
+                        format!(
+                            "Failed to configure wireguard interface {} with wg and {} (wg:",
+                            &wg_name, &wg_conf
+                        ),
+                        wg_conf_wg_status,
+                    )
                 }
             }
 
@@ -67,21 +87,35 @@ fn main() {
             {
                 let move_wg_status = move_wg(&wg_name, &pid);
                 if move_wg_status.success() {
-                    println!("Moved wireguard interface {} to netns of pid: {}.", wg_name, pid);
+                    println!(
+                        "Moved wireguard interface {} to netns of pid: {}.",
+                        wg_name, pid
+                    );
                 } else {
-                    exitmsg(format!("Failed to move wireguard interface {} to netns of pid: {}. (ip:", wg_name, &pid.to_string()), move_wg_status);
+                    exitmsg(
+                        format!(
+                            "Failed to move wireguard interface {} to netns of pid: {}. (ip:",
+                            wg_name,
+                            &pid.to_string()
+                        ),
+                        move_wg_status,
+                    );
                 }
             }
-            
+
             println!("Configuring wireguard in the pid netns.");
             {
                 println!("Spawning netns thread.");
-                let netns_conf_handle = thread::spawn(move || {netns_conf_ip(&wg_name, &wg_addr, &pid)});
-                netns_conf_handle.join().expect("Netns configuration failed.");
+                let netns_conf_handle =
+                    thread::spawn(move || netns_conf_ip(&wg_name, &wg_addr, &pid));
+                netns_conf_handle
+                    .join()
+                    .expect("Netns configuration failed.");
             }
 
-            req_stream.write("Done".as_bytes()).expect("Failed to respond to request.");
-
+            req_stream
+                .write("Done".as_bytes())
+                .expect("Failed to respond to request.");
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,48 @@
-use std::{process::{Command,ExitStatus,id}, sync::mpsc::Receiver, os::unix::{net::{UnixListener,UnixStream},process::CommandExt}, io::{Write,Read}};
 use libc;
+use std::{
+    io::{Read, Write},
+    os::unix::{
+        net::{UnixListener, UnixStream},
+        process::CommandExt,
+    },
+    process::{id, Command, ExitStatus},
+    sync::mpsc::Receiver,
+};
 
 pub fn create_wg(name: &str) -> ExitStatus {
     Command::new("ip")
         .args(["link", "add", "dev", name, "type", "wireguard"])
-        .spawn().expect("Failed run ip, do you have it installed?")
-        .wait().expect("Failed to wait on child, something went very wrong.")
+        .spawn()
+        .expect("Failed run ip, do you have it installed?")
+        .wait()
+        .expect("Failed to wait on child, something went very wrong.")
 }
-
 
 pub fn stay_behind_orig_netns(wg_name: &str, rx: Receiver<()>, pid: u32) {
     println!("Thread will wait in original namespace.");
-    rx.recv().expect("Channel broken to original namespace thread.");
-    println!("Moving wireguard interface {} to netns of pid: {}", wg_name, pid);
+    rx.recv()
+        .expect("Channel broken to original namespace thread.");
+    println!(
+        "Moving wireguard interface {} to netns of pid: {}",
+        wg_name, pid
+    );
     let move_wg_status = move_wg(wg_name, &pid.to_string());
     if move_wg_status.success() {
-        println!("Moved wireguard interface {} to netns of pid: {}.", wg_name, pid);
+        println!(
+            "Moved wireguard interface {} to netns of pid: {}.",
+            wg_name, pid
+        );
     } else {
-        exitmsg(format!("Failed to move wireguard interface {} to netns of pid: {}.", wg_name, pid.to_string()), move_wg_status);
+        exitmsg(
+            format!(
+                "Failed to move wireguard interface {} to netns of pid: {}.",
+                wg_name,
+                pid.to_string()
+            ),
+            move_wg_status,
+        );
     }
 }
-
 
 pub fn exec_sunshine() {
     let err = Command::new("sunshine")
@@ -38,7 +60,7 @@ pub fn exec_moonlight() {
 }
 
 pub fn sunshine_path() -> String {
-    format!("unix:/run/user/{}/pulse/native", unsafe {libc::getuid()})
+    format!("unix:/run/user/{}/pulse/native", unsafe { libc::getuid() })
 }
 
 pub fn unshare_netns() -> i32 {
@@ -53,14 +75,16 @@ pub fn get_err() -> i32 {
     std::io::Error::last_os_error().raw_os_error().unwrap()
 }
 pub fn pause() {
-    unsafe { libc::pause(); }
+    unsafe {
+        libc::pause();
+    }
 }
 
 pub fn create_sock(path: &str) -> std::io::Result<UnixListener> {
     UnixListener::bind(path)
 }
 
-pub fn exitmsg(msg : String, s : ExitStatus) {
+pub fn exitmsg(msg: String, s: ExitStatus) {
     if let Some(n) = s.code() {
         panic!("{} {})", msg, n);
     } else {
@@ -79,36 +103,52 @@ pub fn setns(pid: &str) -> i32 {
 pub fn move_wg(name: &str, pid: &str) -> ExitStatus {
     Command::new("ip")
         .args(["link", "set", "dev", name, "netns", pid])
-        .spawn().expect("Failed run ip, do you have it installed?")
-        .wait().expect("Failed to wait on child, something went very wrong.")
+        .spawn()
+        .expect("Failed run ip, do you have it installed?")
+        .wait()
+        .expect("Failed to wait on child, something went very wrong.")
 }
 
 pub fn wg_conf_wg(name: &str, wgconf: &str) -> ExitStatus {
     Command::new("wg")
         .args(["setconf", name, wgconf])
-        .spawn().expect("Failed to run wg, do you have it installed?")
-        .wait().expect(format!("Failed to configure {} using wg, does {} exist?", name, wgconf).as_str())
+        .spawn()
+        .expect("Failed to run wg, do you have it installed?")
+        .wait()
+        .expect(
+            format!(
+                "Failed to configure {} using wg, does {} exist?",
+                name, wgconf
+            )
+            .as_str(),
+        )
 }
 
 pub fn netns_wg_addr(name: &str, addr: &str) -> ExitStatus {
     Command::new("ip")
         .args(["addr", "add", "dev", name, addr])
-        .spawn().expect("Failed run ip, do you have it installed?")
-        .wait().expect("Failed to wait on child, something went very wrong.")
+        .spawn()
+        .expect("Failed run ip, do you have it installed?")
+        .wait()
+        .expect("Failed to wait on child, something went very wrong.")
 }
 
 pub fn netns_lo_up() -> ExitStatus {
     Command::new("ip")
         .args(["link", "set", "dev", "lo", "up"])
-        .spawn().expect("Failed run ip, do you have it installed?")
-        .wait().expect("Failed to wait on child, something went very wrong.")
+        .spawn()
+        .expect("Failed run ip, do you have it installed?")
+        .wait()
+        .expect("Failed to wait on child, something went very wrong.")
 }
 
 pub fn netns_wg_up(name: &str) -> ExitStatus {
     Command::new("ip")
         .args(["link", "set", "dev", name, "up"])
-        .spawn().expect("Failed run ip, do you have it installed?")
-        .wait().expect("Failed to wait on child, something went very wrong.")
+        .spawn()
+        .expect("Failed run ip, do you have it installed?")
+        .wait()
+        .expect("Failed to wait on child, something went very wrong.")
 }
 
 pub fn get_wireguard(req_type: char) {
@@ -135,7 +175,9 @@ pub fn get_wireguard(req_type: char) {
         println!("Made request for wireguard interface on {}", sock_name);
 
         let mut resp = String::new();
-        req_stream.read_to_string(&mut resp).expect("Failed to read respone.");
+        req_stream
+            .read_to_string(&mut resp)
+            .expect("Failed to read respone.");
         if resp == "Done" {
             println!("Request was completed.")
         } else {
@@ -150,14 +192,21 @@ pub fn netns_conf_ip(wg_name: &str, addr: &str, pid: &str) {
     if s == 0 {
         println!("Moved to namespace of pid: {}", pid);
     } else {
-        panic!("Failed to setns to that of pid: {}, err: {}", pid, get_err());
+        panic!(
+            "Failed to setns to that of pid: {}, err: {}",
+            pid,
+            get_err()
+        );
     }
 
     let netns_wg_addr_status = netns_wg_addr(wg_name, addr);
     if netns_wg_addr_status.success() {
         println!("Set {} addr to {}", wg_name, addr);
     } else {
-        exitmsg(format!("Failed to set {} addr to {}", wg_name, addr), netns_wg_addr_status);
+        exitmsg(
+            format!("Failed to set {} addr to {}", wg_name, addr),
+            netns_wg_addr_status,
+        );
     }
 
     let netns_lo_up_status = netns_lo_up();
@@ -171,7 +220,10 @@ pub fn netns_conf_ip(wg_name: &str, addr: &str, pid: &str) {
     if netns_wg_up_status.success() {
         println!("Set {} status to up", wg_name);
     } else {
-        exitmsg(format!("Failed to set {} status to up", wg_name),netns_wg_up_status);
+        exitmsg(
+            format!("Failed to set {} status to up", wg_name),
+            netns_wg_up_status,
+        );
     }
     println!("Completed netns configuration.")
 }
