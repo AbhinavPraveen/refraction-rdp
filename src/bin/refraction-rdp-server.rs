0f94bf391044ee66da483afe5c410fd07b9eb75f
Abhinav Praveen <abhinav@praveen.org.uk>
2023-06-01 21:44:52 +0100

    Change _ to -.

--- src/bin/refraction-rdp-server.rs
+++ src/bin/refraction-rdp-server.rs
@@ -2,7 +2,7 @@ use refraction_rdp::*;
 
 fn main() {
     let pulse_path = &pulse_path();
-    let wg_name = &"wg_refraction";
+    let wg_name = &"wg-refraction";
     let wg_addr = &"10.10.10.1/24";
 
     get_wireguard('s', wg_name, wg_addr);
